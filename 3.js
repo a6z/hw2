3ec8ce373cd0eebb4ba0c44cb1a88924ddbd7397
a6z <bb840720@gmail.com>
2016-03-04 20:35:02 +0800

    Added files via upload

---
+++ 3.js
@@ -0,0 +1,13 @@
+// function expression
+var greetMe = function() {
+    console.log('Hi A6!');
+}
+greetMe();
+
+// arrow function expression
+var greetSam  = () => console.log('Hello,A6!');
+greetSam();
+
+// arrow function expression with parameters
+var greetYou = name => console.log(`Hi ${name}!`);
+greetYou('Ntue');
